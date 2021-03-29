dc5d87258a95e495064d3b12dc16a5d40ae57dda
Ahmed Ibrahim <ib153507@gmail.com>
2021-03-29 17:17:05 +0200

    ADD action types to filter the property

--- client/src/actions/actionsType.js
+++ client/src/actions/actionsType.js
@@ -4,3 +4,12 @@ export const GET_HOME_PROPERTY_FAIL = 'GET_HOME_PROPERTY_FAIL';
 
 export const PROPERTY_DETAIL_SUCCESS = 'PROPERTY_DETAIL_SUCCESS';
 export const PROPERTY_DETAIL_FAIL = 'PROPERTY_DETAIL_FAIL';
+
+export const ADD_AMENITIES = 'ADD_AMENITIES';
+export const COUNTERY = 'COUNTERY';
+export const PRICE = 'PRICE';
+export const AREA = 'AREA';
+export const BEDS = 'BEDS';
+export const BATHS = 'BATHS';
+export const STATUS = 'STATUS';
+
