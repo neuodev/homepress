6a04fa00d238374272c82ae3b33686188a108b85
Ahmed Ibrahim <ib153507@gmail.com>
2021-03-29 11:18:59 +0200

    Add property reducer

--- client/src/reducers/propery.js
+++ client/src/reducers/propery.js
@@ -1,11 +1,31 @@
-import {gethome} from '../actions/actionsType'
-export const homeProperty = (state = {} , {type, payload} ) => {
-    switch (type) {
-        case GET_HOME_PROPERTY_REQUEST:
-            
-            break;
-    
-        default:
-            break;
-    }
-}
+import {
+  GET_HOME_PROPERTY_FAIL,
+  GET_HOME_PROPERTY_REQUEST,
+  GET_HOME_PROPERTY_SUCCESS,
+} from '../actions/actionsType';
+export const homeProperty = (
+  state = {
+    proprties: [],
+  },
+  { type, payload }
+) => {
+  switch (type) {
+    case GET_HOME_PROPERTY_REQUEST:
+      return {
+        loading: true,
+      };
+    case GET_HOME_PROPERTY_SUCCESS:
+      return {
+        loading: false,
+        proprties: payload,
+      };
+    case GET_HOME_PROPERTY_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: payload,
+      };
+    default:
+      return state;
+  }
+};
