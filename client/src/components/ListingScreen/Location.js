de4e97ad9357265485ab7e7532d6eb7a9f026282
Ahmed Ibrahim <ib153507@gmail.com>
2021-03-29 14:09:00 +0200

    Setup Locaiton and Map

--- client/src/components/ListingScreen/Location.js
+++ client/src/components/ListingScreen/Location.js
@@ -1,24 +1,8 @@
 import React, { useEffect, useRef, useState } from 'react';
-import mapboxgl from 'mapbox-gl/dist/mapbox-gl-csp';
-
-mapboxgl.accessToken =
-  'pk.eyJ1IjoiYWhtZWRpYnJhaGltMTIzIiwiYSI6ImNrbXVoeGgxdzB6Ymoyd3BvYXRndXF0NXAifQ.7QpLOlNKBHpu3HwNNsiJmw';
+import Map from './Map';
 
 const Location = ({ location, address }) => {
-  const mapContainer = useRef();
-  const [lng, setLng] = useState(-70.9166);
-  const [lat, setLat] = useState(42.347);
-  const [zoom, setZoom] = useState(3);
-  useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [lng, lat],
-      zoom: zoom,
-    });
-    return () => map.remove();
-  }, [lng, lat, zoom]);
-
+  
   return (
     <div>
       <div className='container mx-auto max-w-screen-lg p-5 pt-7'>
@@ -31,7 +15,9 @@ const Location = ({ location, address }) => {
             <p>{address}</p>
           </div>
         </div>
-        <div ref={mapContainer} className='w-full h-96 bg-blue-300 mt-7'></div>
+        <div className='w-full h-96 bg-blue-300 mt-7'>
+          {/* <Map /> */}
+        </div>
       </div>
     </div>
   );
