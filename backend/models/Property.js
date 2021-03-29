2a69cbf0047e86fe2223ac8cc075818de57e87cd
Ahmed Ibrahim <ib153507@gmail.com>
2021-03-29 08:15:07 +0200

    update the Property schema

--- backend/models/Property.js
+++ backend/models/Property.js
@@ -42,63 +42,7 @@ const featuresSchema = new Schema({
     required: [true, 'Swimming Pools Number Is requried'],
     default: 0,
   },
-});
-const PropertySchema = new Schema({
-  type: {
-    type: String,
-    requried: [true, 'Property type is requried'],
-    enum: ['Apartments', 'Offices', 'Houses', 'Townhome', 'Studio'],
-  },
-  title: {
-    type: String,
-    required: [true, 'Property Title is required'],
-  },
-  price: {
-    type: Number,
-    required: [true, 'Property price  is required'],
-  },
-  discount: {
-    type: Number,
-    required: true,
-    default: 0,
-  },
-  description: {
-    type: String,
-    required: [true, 'Description is required'],
-  },
-  location: {
-    type: {
-      type: String,
-      enum: ['Point'],
-      required: true,
-    },
-    coordinates: {
-      type: [Number],
-      required: true,
-      index: '2dsphere',
-    },
-    formatedAddress: String,
-  },
-  images: [
-    {
-      type: String,
-      required: [true, 'Images is requries'],
-      trim: true,
-    },
-  ],
-  rooms: {
-    type: Number,
-    required: [true, 'Rooms Number is required'],
-  },
-  beds: {
-    type: Number,
-    required: [true, 'Beds Number is required'],
-  },
-  Baths: {
-    type: Number,
-    required: [true, 'Beds Number is required'],
-  },
-  grage: {
+  garage: {
     type: Number,
     required: [true, 'Beds Number is required'],
   },
@@ -106,55 +50,76 @@ const PropertySchema = new Schema({
     type: Number,
     required: [true, 'Built Year Number is required'],
   },
-  area: {
-    type: Number,
-    required: [true, 'Area  is required'],
-  },
-  priceForSqft: {
-    type: Number,
-    required: [true, 'Rooms Number is required'],
-  },
-  features: featuresSchema,
-  amenities: [{ type: String, required: true }],
-  near: {
-    food: [
-      {
-        name: {
-          type: String,
-          required: [true, 'Restaurant Name is required'],
-        },
-        rating: {
-          type: Number,
-          requried: [true, 'Rating is required'],
-        },
-      },
-    ],
-    education: [
-      {
-        name: {
-          type: String,
-          required: [true, 'Name is required'],
-        },
-        rating: {
-          type: Number,
-          requried: [true, 'Rating is required'],
-        },
-      },
-    ],
-    homeservices: [
-      {
-        name: {
-          type: String,
-          required: [true, 'Name is required'],
-        },
-        rating: {
-          type: Number,
-          requried: [true, 'Rating is required'],
-        },
-      },
-    ],
-  },
 });
+const PropertySchema = new Schema(
+  {
+    type: {
+      type: String,
+      requried: [true, 'Property type is requried'],
+      enum: ['Apartments', 'Offices', 'Houses', 'Townhome', 'Studio'],
+    },
+    title: {
+      type: String,
+      required: [true, 'Property Title is required'],
+    },
+    price: {
+      type: Number,
+      required: [true, 'Property price  is required'],
+    },
+    discount: {
+      type: Number,
+      required: [true, 'Property Discount  is required'],
+      default: 0,
+    },
+    description: {
+      type: String,
+      required: [true, 'Description is required'],
+    },
+    location: {
+      type: {
+        type: String,
+        enum: ['Point'],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+        index: '2dsphere',
+      },
+      formatedAddress: String,
+    },
+    images: [
+      {
+        type: String,
+        required: [true, 'Images is requries'],
+        trim: true,
+      },
+    ],
+    rooms: {
+      type: Number,
+      required: [true, 'Rooms Number is required'],
+    },
+    beds: {
+      type: Number,
+      required: [true, 'Beds Number is required'],
+    },
+    Baths: {
+      type: Number,
+      required: [true, 'Beds Number is required'],
+    },
+    area: {
+      type: Number,
+      required: [true, 'Area  is required'],
+    },
+    priceForSqft: {
+      type: Number,
+      required: [true, 'Rooms Number is required'],
+    },
+    features: featuresSchema,
+    amenities: [{ type: String, required: true }],
+  },
+  { timestamps: true }
+);
 
 const Property = mongoose.model('Property', PropertySchema);
 
