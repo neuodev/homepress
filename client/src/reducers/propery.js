6d6b2e270740d537071f13035379262512309ee0
Ahmed Ibrahim <ib153507@gmail.com>
2021-03-29 11:16:57 +0200

    Update action types

---
+++ client/src/reducers/propery.js
@@ -0,0 +1,11 @@
+import {gethome} from '../actions/actionsType'
+export const homeProperty = (state = {} , {type, payload} ) => {
+    switch (type) {
+        case GET_HOME_PROPERTY_REQUEST:
+            
+            break;
+    
+        default:
+            break;
+    }
+}
