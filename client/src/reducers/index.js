028b16e2560fe2db466935769816db95ce694a5a
Ahmed Ibrahim <ib153507@gmail.com>
2021-03-30 09:19:36 +0200

    Connect searchProperties to root reduer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { homeProperty, getProperty } from './propery';
 import { filter } from './filter';
 import { locations } from './locations';
+import { searchProperties } from './properties';
 export default combineReducers({
   homeProperty,
   getProperty,
   filter,
   locations,
+  searchProperties,
 });
