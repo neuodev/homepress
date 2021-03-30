18b829d69af163bacfac5b988c9ab0416aea5b15
Ahmed Ibrahim <ib153507@gmail.com>
2021-03-30 08:39:12 +0200

    update locations reducer

--- client/src/reducers/locations.js
+++ client/src/reducers/locations.js
@@ -1,11 +1,9 @@
 import { GET_LOCATIONS } from '../actions/actionsType';
 
-export const locations = (state = {}, { type, payload }) => {
+export const locations = (state = [], { type, payload }) => {
   switch (type) {
     case GET_LOCATIONS:
-      return {
-        locations: payload,
-      };
+      return payload;
     default:
       return state;
   }
