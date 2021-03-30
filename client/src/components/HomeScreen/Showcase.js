006ac620a4d1047460204b14fc68d8a4ce7f693e
Ahmed Ibrahim <ib153507@gmail.com>
2021-03-30 13:34:54 +0200

    update showcase.js

--- client/src/components/HomeScreen/Showcase.js
+++ client/src/components/HomeScreen/Showcase.js
@@ -13,12 +13,12 @@ const Showcase = ({ history }) => {
   };
   return (
     <div
-      className='w-full h-72 showcase  flex items-center flex-col '
+      className='w-full h-72 showcase relative flex items-center '
       style={{
         background: 'url(./images/showcase.jpg) no-repeat center center/cover ',
       }}
       id='overlay-showcase'>
-      <div className='p-5 absoluted z-10 w-full top-24 sm:top-0   '>
+      <div className='p-5 absolute z-10 w-full top-24 sm:top-0   '>
         <div className=' flex items-center justify-between container mx-auto'>
           <Link to='/'>
             <img
@@ -30,21 +30,21 @@ const Showcase = ({ history }) => {
           <div className='flex items-center justify-center space-x-3  '>
             <Link
               to='/addListing'
-              className='border border-blue-600 bg-blue-100 text-blue-500 px-3 py-2 rounded-full focus:outline-none focus:ring focus:ring-blue-300  font-semibold '>
+              className='border border-blue-600  bg-blue-100  text-blue-500 px-3 py-2 rounded-full focus:outline-none focus:ring focus:ring-blue-300  font-semibold '>
               Add Listing
             </Link>
             <Favorite>
-              <button className='border border-blue-600 bg-blue-100 text-blue-500 px-3 py-2 rounded-full focus:outline-none focus:ring focus:ring-blue-300   '>
+              <button className='border border-blue-600  bg-blue-100  text-blue-500 px-3 py-2 rounded-full focus:outline-none focus:ring focus:ring-blue-300   '>
                 <i class='fa fa-heart' aria-hidden='true'></i>
               </button>
             </Favorite>
-            <button className='border border-blue-600 bg-blue-100 text-blue-500 px-3 py-2 rounded-full focus:outline-none focus:ring focus:ring-blue-300   '>
+            <button className='border border-blue-600  bg-blue-100  text-blue-500 px-3 py-2 rounded-full focus:outline-none focus:ring focus:ring-blue-300   '>
               <i class='fa fa-retweet' aria-hidden='true'></i>
             </button>
           </div>
         </div>
       </div>
-      <div className=' container mx-auto mt-40   hidden md:block px-5'>
+      <div className=' container mx-auto   hidden md:block px-5'>
         <p className='text-blue-500 text-xl'>PASADENA REALTOR & BROKER</p>
         <h1 className='text-4xl max-w-sm lg:text-6xl  lg:max-w-xl  font-extrabold text-gray-800 '>
           Making Good things happen.
