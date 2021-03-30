f3076336e9aa3cf9a281e57226769f982a4b120d
Ahmed Ibrahim <ib153507@gmail.com>
2021-03-30 12:26:03 +0200

    connect favoriteList reducer to the root reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,10 +3,12 @@ import { homeProperty, getProperty } from './propery';
 import { filter } from './filter';
 import { locations } from './locations';
 import { searchProperties } from './properties';
+import { favoriteList } from './favorite';
 export default combineReducers({
   homeProperty,
   getProperty,
   filter,
   locations,
   searchProperties,
+  favoriteList,
 });
