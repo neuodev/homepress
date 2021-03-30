ca87be2ee26557285575938566c1114b0c136cfb
Ahmed Ibrahim <ib153507@gmail.com>
2021-03-30 08:32:11 +0200

    Connect location reducer with  root reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { homeProperty, getProperty } from './propery';
 import { filter } from './filter';
+import { locations } from './locations';
 export default combineReducers({
   homeProperty,
   getProperty,
   filter,
+  locations,
 });
