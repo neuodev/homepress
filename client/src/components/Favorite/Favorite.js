980bf7e1fcc015293bad035898a24631618ee5a2
Ahmed Ibrahim <ib153507@gmail.com>
2021-03-30 15:26:21 +0200

    Add favorite card

--- client/src/components/Favorite/Favorite.js
+++ client/src/components/Favorite/Favorite.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import FavoriteCard from './FavoriteCard';
 const Favorite = ({ children }) => {
   const { favoriteItems } = useSelector(state => state.favoriteList);
+  const [show, setShow] = useState(true);
+
+  document.addEventListener('click', e => {
+    if (!e.target.closest('#favorite-list')) {
+      setShow(false);
+    }
+  });
   return (
-    <div className=''>
+    <div className='relative' id='favorite-list'>
       <div className='relative'>
         <p
           className='absolute -top-2 -right-2 bg-blue-100  rounded-full text-blue-600
@@ -12,7 +19,21 @@ const Favorite = ({ children }) => {
         '>
           {favoriteItems.length}
         </p>
-        {children}
+        <p onClick={() => setShow(!show)}> {children}</p>
+      </div>
+      <div
+        className={`${
+          show ? 'showFavoriteList' : 'hideFavoriteList'
+        } top-12 right-0 absolute 
+      w-96 bg-gray-50 shadow-xl
+      `}>
+        <div className='px-5 py-7 '>
+          {favoriteItems.map((property, idx) => (
+            <div key={idx}>
+              <FavoriteCard property={property} />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- client/src/components/Favorite/FavoriteCard.js
+++ client/src/components/Favorite/FavoriteCard.js
@@ -1,11 +1,55 @@
-import React from 'react'
-
-const FavoriteCard = () => {
-    return (
-        <div>
-            <h1>card</h1>
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { FAVORITE_REMOVE_ITEM } from '../../actions/actionsType';
+const FavoriteCard = ({ property }) => {
+  const { title, images, price, discount, id, beds, baths } = property;
+  const newPrice = price - price * (discount / 100);
+  const dispatch = useDispatch();
+  const onClick = () => {
+    dispatch({ type: FAVORITE_REMOVE_ITEM, payload: id });
+  };
+  return (
+    <div className='grid grid-cols-12 grid-rows-6 gap-4 mb-5 h-28 '>
+      <div className='col-span-6 row-span-full '>
+        <Link to={`/listing/${id}`}>
+          <img
+            className='h-full w-full inline-block object-cover'
+            src={images[0]}
+            alt={title}
+          />
+        </Link>
+      </div>
+      <div className='col-span-6 h-full'>
+        <Link to={`/listing/${id}`}>
+          <h1 className='truncate text-lg  font-semibold '>{title}</h1>
+        </Link>
+        <div className='flex  items-baseline'>
+          <p className='text-lg  mr-1 font-semibold text-gray-600 '>
+            ${newPrice}
+          </p>
+          <p className='text-sm line-through '>${price}</p>
         </div>
-    )
-}
+        <div className='col-span-6'>
+          <div className='flex items-center '>
+            <i className='far fa-bed  text-gray-500 mr-2 '></i>
+            <p>{beds} Beds</p>
+          </div>
+          <div className='flex items-center justify-between'>
+            <div className='flex items-center '>
+              <i className='far fa-bath  text-gray-500 mr-2 '></i>
+              <p>{baths} Baths</p>
+            </div>
+            <button
+              onClick={onClick}
+              className='focus:outline-none text-red-400'>
+              <i class='far fa-trash' aria-hidden='true'></i>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default FavoriteCard
+export default FavoriteCard;
