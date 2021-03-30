cab35ccb7f33ce3a211e762ceb47289694195e1b
Ahmed Ibrahim <ib153507@gmail.com>
2021-03-30 08:13:42 +0200

    Add Markers to the Map

--- client/src/components/SearchScreen/Map.js
+++ client/src/components/SearchScreen/Map.js
@@ -2,12 +2,6 @@ import React, { useEffect, useRef, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-const styles = {
-  width: '100%',
-  height: '100%',
-  position: 'block',
-};
-
 const MapboxGLMap = ({ location }) => {
   location = [20, 70];
   const [map, setMap] = useState(null);
@@ -20,23 +14,63 @@ const MapboxGLMap = ({ location }) => {
       const map = new mapboxgl.Map({
         container: mapContainer.current,
         style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-        center: [location[0], location[1]],
-        zoom: 10,
+        center: [0, 0],
+        zoom: 4,
       });
 
       map.on('load', () => {
         setMap(map);
         map.resize();
-        var marker = new mapboxgl.Marker()
-          .setLngLat([location[0], location[1]])
-          .addTo(map);
+        map.loadImage(
+          'https://upload.wikimedia.org/wikipedia/commons/f/f2/678111-map-marker-512.png',
+
+          function (error, image) {
+            if (error) {
+              console.log(image);
+              alert('error');
+              throw error;
+            }
+            map.addImage('mapMarker', image);
+            map.addSource('point', {
+              type: 'geojson',
+              data: {
+                type: 'FeatureCollection',
+                features: [
+                  {
+                    type: 'Feature',
+                    geometry: {
+                      type: 'Point',
+                      coordinates: [0, 0],
+                    },
+                  },
+                ],
+              },
+            });
+            map.addLayer({
+              id: 'points',
+              type: 'symbol',
+              source: 'point',
+              layout: {
+                'icon-image': 'mapMarker',
+                'icon-size': 0.15,
+              },
+            });
+          }
+        );
       });
     };
 
     if (!map) initializeMap({ setMap, mapContainer });
   }, [map]);
 
-  return <div ref={el => (mapContainer.current = el)} style={styles} />;
+  return (
+    <div className='col-span-12 px-5  lg:col-span-6 lg:px-0 ' id='map-height'>
+      <div
+        className=' absoluted dtop-0 dleft-0  w-full h-full'
+        ref={el => (mapContainer.current = el)}
+      />
+    </div>
+  );
 };
 
 export default MapboxGLMap;
