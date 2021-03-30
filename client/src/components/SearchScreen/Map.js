2083ca302d740d7ef24b6afac4526233d3fa1170
Ahmed Ibrahim <ib153507@gmail.com>
2021-03-30 07:30:42 +0200

    Add Map to the searchScreen.js

--- client/src/components/SearchScreen/Map.js
+++ client/src/components/SearchScreen/Map.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
-const Map = () => {
-  return (
-    <div
-      className='w-full h-full bg-blue-200 col-span-12 lg:col-span-6'
-      id='grid-h'>
-
-    </div>
-  );
+const styles = {
+  width: '100%',
+  height: '100%',
+  position: 'block',
 };
 
-export default Map;
+const MapboxGLMap = ({ location }) => {
+  location = [20, 70];
+  const [map, setMap] = useState(null);
+  const mapContainer = useRef(null);
+
+  useEffect(() => {
+    mapboxgl.accessToken =
+      'pk.eyJ1IjoiYWhtZWRpYnJhaGltMTIzIiwiYSI6ImNrbXVoeGgxdzB6Ymoyd3BvYXRndXF0NXAifQ.7QpLOlNKBHpu3HwNNsiJmw';
+    const initializeMap = ({ setMap, mapContainer }) => {
+      const map = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+        center: [location[0], location[1]],
+        zoom: 10,
+      });
+
+      map.on('load', () => {
+        setMap(map);
+        map.resize();
+        var marker = new mapboxgl.Marker()
+          .setLngLat([location[0], location[1]])
+          .addTo(map);
+      });
+    };
+
+    if (!map) initializeMap({ setMap, mapContainer });
+  }, [map]);
+
+  return <div ref={el => (mapContainer.current = el)} style={styles} />;
+};
+
+export default MapboxGLMap;
